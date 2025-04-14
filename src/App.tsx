b5687491893afd1df2ef71eef260f1643124f500
Abhishek Thormothe <117808499+abhi2k4@users.noreply.github.com>
2025-04-14 14:33:36 +0530

    insights

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -22,9 +20,8 @@ const App = () => (
         duration={5000}
       />
       <BrowserRouter>
+      <Analytics />
         <Routes>
-          <Analytics />
-          <SpeedInsights />
           {/* ADD ALL CUSTOM ROUTES BELOW THE CATCH-ALL "*" ROUTE */}
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
