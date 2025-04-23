2aab6070411544b942d09b02059524dbf562b2b6
Abhishek Thormothe <117808499+abhi2k4@users.noreply.github.com>
2025-04-24 02:23:48 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,19 @@ import { LikeButton } from "./LikeButton";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
   
   return (
-    <footer className="border-t py-8 bg-secondary/50">
+    <footer className="border-t py-8 bg-secondary/20">
       <div className="container mx-auto px-4">
+        
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="flex items-center gap-2 mb-4 md:mb-0">
             <Logo/>
-            <span className="font-bold text-lg">Abhishek's Portfolio</span>
+            <span className="font-bold text-lg font-audiowide">abhi codes</span>
           </div>
           <div className="flex gap-6 mb-4 mt-4 animate-fade-in">
             <LikeButton />
